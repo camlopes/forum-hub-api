0d53b73d8ffa236e235b4f48c876f354875029bc
Camila Lopes <63123059+camlopes@users.noreply.github.com>
2025-07-25 17:47:26 -0300

    Campos nome e email da migration de usuario são unicos

--- src/main/resources/db/migration/V1__create-table-usuarios.sql
+++ src/main/resources/db/migration/V1__create-table-usuarios.sql
@@ -1,8 +1,8 @@
 create table usuarios(
 
     id bigint not null auto_increment,
-    nome varchar(100) not null,
-    email varchar(100) not null,
+    nome varchar(100) not null unique,
+    email varchar(100) not null unique,
     senha varchar(255) not null,
     perfil varchar(100) not null,
 
