13c574e8f1dcb8fbb33f586b0c9f5131729518f9
Camila Lopes <63123059+camlopes@users.noreply.github.com>
2025-07-29 12:13:58 -0300

    Classe para tratar os possiveis erros na API

---
+++ src/main/java/forum/hub/api/infra/exception/TratadorDeErros.java
@@ -0,0 +1,33 @@
+package forum.hub.api.infra.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler({
+            EntityNotFoundException.class,
+            MethodArgumentTypeMismatchException.class
+    })
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(DadosErrosValidacao::new).toArray());
+    }
+
+    private record DadosErrosValidacao(String campo, String mensagem) {
+        public DadosErrosValidacao(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
